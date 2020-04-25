0e0f5fb9fd71043e274a07c2e019150ddda3cc39
melissa-murphy <melissamurphy.ca@gmail.com>
2020-04-24 17:59:21 -0700

    serializers for users and room groups

---
+++ hiatus_backend/api/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'user_name', 'email', 'password', 'current_room', 'health_pts', 'phrase']
+        
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'current_room']
