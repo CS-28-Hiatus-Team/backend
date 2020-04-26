c27936d5b532c8db97c0709614ccfdde9c498c7f
melissa-murphy <melissamurphy.ca@gmail.com>
2020-04-26 13:31:05 -0700

    created root for static files

--- hiatus_backend/settings.py
+++ hiatus_backend/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Database config for heroku
 db_from_env = dj_database_url.config(conn_max_age=500)
