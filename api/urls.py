8e6e38bbc7e894ee2210dce04ba1c8db3cd2ddaf
melissa-murphy <melissamurphy.ca@gmail.com>
2020-04-28 09:53:46 -0700

    big dump, no seed data yet

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,21 @@
+
 from django.urls import path, include, re_path
-from django.conf.urls import url
+from rest_framework.authtoken import views
 from rest_framework import routers
-from api import views
+from .views import UserViewSet, GroupViewSet, RoomViewSet, PlayerViewSet
 
-# Router Patterns
+# TODO: break out router for adv views to another app
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'rooms', RoomViewSet)
+router.register(r'players', PlayerViewSet)
 
-# URL Patterns
 urlpatterns = [
-    path('', include('rest_auth.urls')),
+    path('auth/', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
+    re_path(r'^api-token-auth/', views.obtain_auth_token),
+    path('', include(router.urls)),
+    path('rooms/', RoomViewSet),
+    path('players/', PlayerViewSet)
 ]
