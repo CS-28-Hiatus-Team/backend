8b59009ba655e7903d3382e20ed7376fdbaec125
melissa-murphy <melissamurphy.ca@gmail.com>
2020-04-29 20:06:20 -0700

    cleaned auth, split adv to new app

--- hiatus_backend/urls.py
+++ hiatus_backend/urls.py
@@ -1,11 +1,12 @@
 """hiatus_backend URL Configuration
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf.urls import include
 
 # URL Patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    # path('api/adv/', include('adventure.urls'))
 ]
