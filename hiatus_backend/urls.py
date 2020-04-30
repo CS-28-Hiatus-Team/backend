19b66f6ef365440586202107a05d50a9af7a6282
melissa-murphy <melissamurphy.ca@gmail.com>
2020-04-30 10:22:21 -0700

    remove django.conf.urls

--- hiatus_backend/urls.py
+++ hiatus_backend/urls.py
@@ -2,7 +2,7 @@
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import include
+# from django.conf.urls import include
 
 # URL Patterns
 urlpatterns = [
